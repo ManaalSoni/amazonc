fb28d443022fc3c13a818eaaaeba4c7cac61a8d5
codester911 <72037462+codester911@users.noreply.github.com>
2021-09-08 07:38:57 +0530

    cors added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const routeController = require("./api/v1");
-
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
